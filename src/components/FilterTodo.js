7f12e295b5224e66e17e3cabfe8fc251db10fb61
katinbox <tranquanki28@gmail.com>
2022-08-16 15:17:41 +0700

    refact: add clear filter

--- src/components/FilterTodo.js
+++ src/components/FilterTodo.js
@@ -1,19 +1,22 @@
 import useTodo from "../hooks/useTodo";
 import { filterTodo } from "../store/todoActions";
 const FilterTodo = () => {
-  const [_, dispatch] = useTodo();
+  const [state, dispatch] = useTodo();
+  const { query } = state;
 
   const handleFilter = (e) => {
     const { value } = e.target;
     dispatch(filterTodo(value.toLowerCase()));
   };
 
+  const clearFilter = () => dispatch(filterTodo(""));
+
   return (
     <div className="todo-filter">
       <h3>Filter task(by name)</h3>
       <div className="input">
-        <input type="search" onChange={handleFilter} />
-        <button>Clear</button>
+        <input type="search" onChange={handleFilter} value={query} />
+        <button onClick={clearFilter}>Clear</button>
       </div>
     </div>
   );
